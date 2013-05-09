f41b6bb3e9fee1bb273f6f1095f60d737b0af68e
kyle shay <kyle.shay@ymail.com>
2013-05-09 01:26:49 -0400

    Update midimap.js
    added some error handling if midi can't be loaded, and no longer require value to be passed to map function

--- midimap.js
+++ midimap.js
@@ -19,7 +19,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-function midimap() {
+function midi() {
   // private vars
   var mappings = {};
   var lookup = {};
@@ -35,17 +35,17 @@ function midimap() {
 
   navigator.requestMIDIAccess(function(midiAccess) {
     console.log("MIDI ready!");
-    var midi = midiAccess;
+    var m = midiAccess;
 
     // find and print all inputs
-    var inputs = midi.getInputs();
+    var inputs = m.getInputs();
     console.log(inputs.length + " inputs:");
     for (var i = 0; i < inputs.length; i++)
       console.log(i + ": " + inputs[i]);
 
     // just attach the first input
     if (inputs.length > 0) {
-      midi.getInput(inputs[0]).addEventListener("message", function(e) {
+      m.getInput(inputs[0]).addEventListener("message", function(e) {
         var control = e.data[1];
 
         if (mapId) {
@@ -61,14 +61,15 @@ function midimap() {
       });
       console.log("Hooked up first input.");
     }
-  });
+  }, function() { console.log("error loading midi"); });
 
   // privileged methods
   this.add = function(id) {
     mapId = id;
   }
   this.get = function(id) {
-    return lookup[id].value;
+  	if(!lookup[id]) return 0;
+    return lookup[id].value || 0;
   }
   this.map = function(m) {
     mappings = m;
